e7b90143d22c63d3b9c0bf0795a51743d002758f
mihiragarwal1 <72967633+mihiragarwal1@users.noreply.github.com>
2023-09-18 16:36:54 +0530

    throttle feathering

--- Code_Main/Program/IO/carDrive.py
+++ Code_Main/Program/IO/carDrive.py
@@ -2,6 +2,7 @@ import Jetson.GPIO as GPIO
 from threading import Thread
 from IO import io
 import time
+import math
 
 # drive module for controlling throttle and steering output
 
@@ -20,8 +21,8 @@ strMAX = 47
 strMIN = 28
 strTRIM = 8
 # throttle feathering
-# thrFeaFREQ = 10
-# thrFeaDiv = 20
+thrFeaFREQ = 10
+thrFeaDiv = 20
 # control variables
 targetThrottle = 0
 targetSteering = 0
@@ -42,27 +43,27 @@ def start():
         s.start((strMIN+strMAX)/2)
         def loop():
             try:
-                # global running, t, s, currThrottle, currSteering, targetThrottle, targetSteering, thrFeaFREQ, thrFeaDiv, tickrate
-                global running, t, s, currThrottle, currSteering, targetThrottle, targetSteering, steerSpeed, tickrate
+                global running, t, s, currThrottle, currSteering, targetThrottle, targetSteering, thrFeaFREQ, thrFeaDiv, tickrate
+                # global running, t, s, currThrottle, currSteering, targetThrottle, targetSteering, steerSpeed, tickrate
                 # timer = 0
                 while running:
                     start = time.time()
                     # convert throttle to active time
-                    # thrFeaACT = math.floor(abs(targetThrottle)/20)/thrFeaDiv
-                    # if timer >= 1: timer = 0
-                    # if timer <= thrFeaACT and thrFeaACT < 1 and targetThrottle > 10: currThrottle = 100
-                    # elif targetThrottle < -10 or thrFeaACT >= 1: currThrottle = targetThrottle
-                    # else: currThrottle = 0
+                    thrFeaACT = math.floor(abs(targetThrottle)/20)/thrFeaDiv
+                    if timer >= 1: timer = 0
+                    if timer <= thrFeaACT and thrFeaACT < 1 and targetThrottle > 10: currThrottle = 100
+                    elif targetThrottle < -10 or thrFeaACT >= 1: currThrottle = targetThrottle
+                    else: currThrottle = 0
                     currThrottle = targetThrottle
                     currSteering = targetSteering*steerSpeed + currSteering*(1-steerSpeed)
                     # apply throttle and steering
                     if (currThrottle < 0): t.ChangeDutyCycle((currThrottle/100)*(thrMIN-thrBACK)+thrMIN)
                     else: t.ChangeDutyCycle((currThrottle/100)*(thrMAX-thrMIN)+thrMIN)
-                    # if (currThrottle < 0): GPIO._set_pwm_duty_cycle(t._ch_info, (currThrottle/100)*(thrMIN2-thrBACK2)+thrMIN2)
-                    # else: GPIO._set_pwm_duty_cycle(t._ch_info, (currThrottle/100)*(thrMAX-thrMIN)+thrMIN)
+                    if (currThrottle < 0): GPIO._set_pwm_duty_cycle(t._ch_info, (currThrottle/100)*(thrMIN2-thrBACK2)+thrMIN2)
+                    else: GPIO._set_pwm_duty_cycle(t._ch_info, (currThrottle/100)*(thrMAX-thrMIN)+thrMIN)
                     s.ChangeDutyCycle((currSteering/100.0)*((strMAX-strMIN)/2)+((strMIN+strMAX)/2)+(strTRIM/10))
                     # advance timer
-                    # timer += thrFeaFREQ/tickrate
+                    timer += thrFeaFREQ/tickrate
                     time.sleep(max((1/tickrate)-(time.time()-start), 0))
             except Exception as err:
                 print(err)
